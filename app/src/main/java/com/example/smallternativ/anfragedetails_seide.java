eb0bb1dc834b3974058a437d57fb863dfb3b6f46
Alexander19098 <Alexander19098>
2020-06-16 13:16:38 +0200

    ich habe gepush auf Alex

---
+++ app/src/main/java/com/example/smallternativ/anfragedetails_seide.java
@@ -0,0 +1,17 @@
+package com.example.smallternativ;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class anfragedetails_seide {
+    Button abbrechenButton;
+
+
+
+
+    };
+
+
+
