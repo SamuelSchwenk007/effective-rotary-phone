164d67e1c5c12e906a9c60d34360221c25059388
Samu <samuel-schwenk@web.de>
2020-07-01 01:30:04 +0200

    fixt kategorie button in fav

--- app/src/main/java/com/example/smallternativ/FavorisierteLaedenFragment.java
+++ app/src/main/java/com/example/smallternativ/FavorisierteLaedenFragment.java
@@ -3,21 +3,28 @@ package com.example.smallternativ;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.view.ContextThemeWrapper;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FavorisierteLaedenFragment extends Fragment implements MyAdapter.ShopListItemListener {
+public class FavorisierteLaedenFragment extends Fragment implements MyAdapter.ShopListItemListener,PopupMenu.OnMenuItemClickListener  {
     private View view;
     private androidx.appcompat.app.ActionBar supportActionBar;
     private RecyclerView recyclerView;
     private MyAdapter.ShopListItemListener shopListItemListener;
+    private Button karte;
+    private Button kategorien;
+    StartseiteActivity startseiteActivity;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,6 +34,20 @@ public class FavorisierteLaedenFragment extends Fragment implements MyAdapter.Sh
         recyclerView = view.findViewById(R.id.recyclerView_favlaeden);
         MyAdapter myAdapter = MyAdapter.getInstance(getActivity().getApplicationContext(), shopListItemListener);
         loadShopsToList(getActivity().getApplicationContext(),myAdapter);
+        karte = view.findViewById(R.id.karte_fav);
+        karte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        kategorien = view.findViewById(R.id.kategorie_fav);
+        kategorien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showKategoriePopup(view);
+            }
+        });
         return view;
     }
     public ActionBar getSupportActionBar() {
@@ -60,6 +81,13 @@ public class FavorisierteLaedenFragment extends Fragment implements MyAdapter.Sh
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(myAdapter);
     }
+    public void showKategoriePopup(View v){
+        Context wrapper = new ContextThemeWrapper(getContext(), R.style.YOURSTYLE);
+        PopupMenu popupMenu = new PopupMenu(wrapper,v);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.kategorie_popup);
+        popupMenu.show();
+    }
 
     @Override
     public void OnShopListItemClick(int position) {
@@ -68,4 +96,49 @@ public class FavorisierteLaedenFragment extends Fragment implements MyAdapter.Sh
     public View getFavFragmentView(){
         return  view;
     }
+    public StartseiteActivity getStartseiteActivity() {
+        return startseiteActivity;
+    }
+
+    public void setStartseiteActivity(StartseiteActivity startseiteActivity) {
+        this.startseiteActivity = startseiteActivity;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.lebensmittel:
+                createStartseiteFragmentWithDeleats("Lebensmittel");
+
+                return true;
+            case R.id.kleidung:
+                createStartseiteFragmentWithDeleats("Kleidung");
+                return true;
+            case R.id.dekoration:
+                createStartseiteFragmentWithDeleats("Dekoration");
+                return true;
+            case R.id.nur_barrierefrei:
+                createStartseiteFragmentWithDeleats("Barrierefrei");
+                return true;
+            case R.id.max_Entfernung:
+                createStartseiteFragmentWithDeleats("MaxEntfernung");
+                return true;
+            default:
+                return false;
+        }
+    } public void createStartseiteFragmentWithDeleats(String kategorie){
+        startseiteActivity.getBlacklineone().setVisibility(View.VISIBLE);
+        startseiteActivity.getBlacklinetwo().setVisibility(View.INVISIBLE);
+        startseiteActivity.getBlacklinetree().setVisibility(View.INVISIBLE);
+        startseiteActivity.getBlacklinefour().setVisibility(View.INVISIBLE);
+        startseiteActivity.getBlacklinefive().setVisibility(View.INVISIBLE);
+        StartseitenFragment startseitenFragment1 = new StartseitenFragment();
+        startseitenFragment1.setKategorie(kategorie);
+        startseitenFragment1.setDeleat(true);
+        startseitenFragment1.setStartseiteActivity(startseiteActivity);
+        startseitenFragment1.setSupportActionBar(getSupportActionBar());
+        startseiteActivity.createFragment(startseitenFragment1);
+        StartseiteActivity.setAppTitle(getSupportActionBar(), getContext(),"Smallternative");
+    }
+
 }
